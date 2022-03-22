c589e4fb07c37c4981cfaa9d041089a1b6120efd
влад тюрин <vladislav-tyurin@bk.ru>
2022-03-22 14:49:44 +0300

    Изменение анимации на главном экране

---
+++ BookAndShop/Services/LayoutServices/CountService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookAndShop.LayoutServices
+{
+    public static class CountService
+    {
+        public static int Count { get; set; } = 0;
+    }
+}
