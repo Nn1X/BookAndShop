fc3a94e501fb10f0457de59929ce9463974d4645
Владислав Тюрин <aryrate@gmail.com>
2022-05-22 00:18:46 +0300

    Добавление жанров в настройки и настройки книг

--- BookAndShop/Models/MyDbContext.cs
+++ BookAndShop/Models/MyDbContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace BookAndShop.Models
 {
@@ -25,7 +22,6 @@ namespace BookAndShop.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseNpgsql("Host=localhost; Port=5432; User Id=postgres; Password=sa; Database=BookShop");
             }
         }
@@ -73,7 +69,6 @@ namespace BookAndShop.Models
             modelBuilder.Entity<Genre>(entity =>
             {
                 entity.Property(e => e.Id)
-                    .ValueGeneratedNever()
                     .HasColumnName("id");
 
                 entity.Property(e => e.Name).HasColumnType("character varying");
